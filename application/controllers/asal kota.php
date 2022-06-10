02f86b42ab99db8e3f7aff74dc8ca9c9ae0c6c75
m fitrah <rajwaizati12@gmail.com>
2022-06-10 15:09:00 +0700

    upadte iril

---
+++ application/controllers/asal kota.php
@@ -0,0 +1,10 @@
+<?php 
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Asal extends CI_Controller {
+
+      public function halaman_tambah(){
+        $this->load->view('halaman_tambah');
+    }
+
+}
